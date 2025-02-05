032da8df2168e3191652755f6c4bb8010749880c
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-05 19:56:32 +0000

    page-routing

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    console.log("This is aboout us");
+    return (
+        <main>
+            <h1>About Us</h1>
+        </main>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 export default function Home() {
+  console.log("This is server component..");
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
